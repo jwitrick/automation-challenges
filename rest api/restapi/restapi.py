190ce924ea4dc227541b930c97d6dcb82769478b
Justin Witrick <justin.witrick@mailtrust.com>
2014-12-09 11:39:02 -0500

    Added another check to ensure data and key are the same

--- rest api/restapi/restapi.py
+++ rest api/restapi/restapi.py
@@ -91,6 +91,8 @@ def find_word(key):
 def create_word(key):
     if ' ' in request.data['word']:
         return {"ERROR": "Failed it must be one word"}
+    if request.data['word'] != key:
+        return {"ERROR": "Word and data mismatch"}
     _add_word(key)
     return '', status.HTTP_204_NO_CONTENT
 
